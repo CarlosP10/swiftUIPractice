172b14a56f48f81ce9980f7554717aa8ae0426b2
Carlos Paredes <00028516@uca.edu.sv>
2023-10-08 05:51:08 -0600

    Learning how to load a remote image from internet

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Carlos Paredes on 30/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
