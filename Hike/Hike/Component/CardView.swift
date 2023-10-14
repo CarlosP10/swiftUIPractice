5b358e326619599829fddcfe147a6122f750d014
Carlos Paredes <00028516@uca.edu.sv>
2023-10-13 18:35:51 -0600

    [UPDATE] - Organization and CardComponent

---
+++ Hike/Hike/Component/CardView.swift
@@ -0,0 +1,42 @@
+//
+//  CardView.swift
+//  Hike
+//
+//  Created by Carlos Paredes on 13/10/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        //MARK: - CARD
+        ZStack {
+            CustomBackgroundView()
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            colors: [
+                                Color("ColorIndigoMedium"),
+                                Color("ColorSalmonLight")
+                            ],
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        )
+                    )
+                    .frame(width: 256, height: 256)
+                    //with frame we set the circle size smaller
+                Image("image-1")
+                    .resizable()
+                .scaledToFit()
+            }
+        }// :CARD
+        .frame(width: 320, height: 570)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
---
+++ Hike/Hike/Utility/K.swift
@@ -0,0 +1,29 @@
+//
+//  K.swift
+//  Hike
+//
+//  Created by Carlos Paredes on 13/10/23.
+//
+import Foundation
+
+
+struct K {
+    struct Colors {
+        static let colorGrayLight = "ColorGrayLight"
+        static let colorGrayMedium = "ColorGrayMedium"
+        static let colorGreenDark = "ColorGreenDark"
+        static let colorGreenLight = "ColorGreenLight"
+        static let colorGreenMedium = "ColorGreenMedium"
+        static let colorIndigoMedium = "ColorIndigoMedium"
+        static let colorSalmonLight = "ColorSalmonLight"
+    }
+    
+    struct Images {
+        static let image1 = "image-1"
+        static let image2 = "image-2"
+        static let image3 = "image-3"
+        static let image4 = "image-4"
+        static let image5 = "image-5"
+    }
+        
+}
