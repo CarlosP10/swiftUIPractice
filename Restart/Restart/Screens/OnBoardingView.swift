0178e1e3146b925a970c6ac505dde7f0e5b1ee27
Carlos Paredes <00028516@uca.edu.sv>
2023-10-23 19:47:30 -0600

    [UPDATE] - Animations in SwiftUI

--- Restart/Restart/Screens/OnBoardingView.swift
+++ Restart/Restart/Screens/OnBoardingView.swift
@@ -13,6 +13,7 @@ struct OnBoardingView: View {
   
   @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
   @State private var buttonOffset: CGFloat = 0
+  @State private var isAnimating: Bool = false
   
   //MARK: - BODY
   var body: some View {
@@ -41,14 +42,20 @@ struct OnBoardingView: View {
           .multilineTextAlignment(.center)
           .padding(.horizontal, 10)
         } //: HEADER
+        .opacity(isAnimating ? 1 : 0)
+        .offset(y: isAnimating ? 0 : -40)
+        .animation(.easeOut(duration: 1), value: isAnimating)
         
         //MARK: - CENTER
+        
         ZStack {
           CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
           
           Image(K.Images.character1)
             .resizable()
             .scaledToFit()
+            .opacity(isAnimating ? 1 : 0)
+            .animation(.easeOut(duration: 0.5), value: isAnimating)
         } // :CENTER
         
         Spacer()
@@ -107,11 +114,13 @@ struct OnBoardingView: View {
                   }
                 }
                 .onEnded { _ in
-                  if buttonOffset > buttonWidth / 2 {
-                    buttonOffset = buttonWidth - 80
-                    isOnBoardingViewActive = false
-                  } else {
-                    buttonOffset = 0
+                  withAnimation(Animation.easeOut(duration: 0.4)) {
+                    if buttonOffset > buttonWidth / 2 {
+                      buttonOffset = buttonWidth - 80
+                      isOnBoardingViewActive = false
+                    } else {
+                      buttonOffset = 0
+                    }
                   }
                 }
             ) //:GESTURE
@@ -122,9 +131,15 @@ struct OnBoardingView: View {
         } //:FOOTER
         .frame(width: buttonWidth, height: 80, alignment: .center)
         .padding()
+        .opacity(isAnimating ? 1 : 0)
+        .offset(y: isAnimating ? 0 : 40)
+        .animation(.easeOut(duration: 1), value: isAnimating)
         
       } //: VSTACK
     } //: ZSTACK
+    .onAppear {
+      isAnimating = true
+    }
   }
 }
 
