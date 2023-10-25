bee96e427b01c9b1bef9d3408024a8f887f419ae
Carlos Paredes <00028516@uca.edu.sv>
2023-10-24 21:21:51 -0600

    [ADD] - Setting up new project

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Carlos Paredes on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pinch/Pinch/Utilities/K.swift
@@ -0,0 +1,17 @@
+//
+//  K.swift
+//  Pinch
+//
+//  Created by Carlos Paredes on 24/10/23.
+//
+
+import Foundation
+
+struct K {
+  struct Image {
+    static let magazineBackCover = "magazine-back-cover"
+    static let magazineFrontCover = "magazine-front-cover"
+    static let thumbMagazineBackCover = "thumb-magazine-back-cover"
+    static let thumbMagazineFrontCover = "thumb-magazine-front-cover"
+  }
+}
