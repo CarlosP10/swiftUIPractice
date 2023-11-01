09a2bc44c1c9986071cd5248cf34b0b75d1e5ba6
Carlos Paredes <00028516@uca.edu.sv>
2023-11-01 17:21:19 -0600

    [UPDATE] - SF Symbol Rendering Mode, SwiftUI Background Material, Long Press Gesture

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -8,71 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
-  //MARK: - PROPERTIES
-  
-  @State private var isAnimating: Bool = false
-  @State private var imageScale: CGFloat = 1
-  @State private var imageOffset: CGSize = .zero
-  
-  //MARK: - FUNCTION
-  func resetImageState() {
-    return withAnimation(.spring()) {
-      imageScale = 1
-      imageOffset = .zero
-    }
-  }
-  
-  //MARK: - CONTENT
-  
-    var body: some View {
-      NavigationView {
-        ZStack {
-          //MARK: - PAGE IMAGE
-          Image(K.Image.magazineFrontCover)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .cornerRadius(10)
-            .padding()
-            .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
-            .opacity(isAnimating ? 1 : 0)
-            .offset(x: imageOffset.width, y: imageOffset.height)
-            .scaleEffect(imageScale)
-            //MARK: - 1. TAP GESTURE
-            .onTapGesture(count: 2) {
-              if imageScale == 1 {
-                withAnimation(.spring()) {
-                  imageScale = 5
-                }
-              } else {
-                resetImageState()
-              }
-            }
-          //MARK: - 2. DRAG GESTURE
-            .gesture(
-              DragGesture()
-                .onChanged { value in
-                  withAnimation(.linear(duration: 1)) {
-                    imageOffset = value.translation
-                  }
-                }
-                .onEnded { _ in
-                  if imageScale <= 1 {
-                    resetImageState()
-                  }
-                }
-            )
-          //MARK: - 2. DRAG GESTURE
-        } //:ZSTACK
-        .navigationTitle("Pinch & Zoom")
-        .navigationBarTitleDisplayMode(.inline)
-        .onAppear {
-          withAnimation(.linear(duration: 1)) {
-            isAnimating = true
-          }
+    //MARK: - PROPERTIES
+    
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    //MARK: - FUNCTION
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
         }
-        
-      } //:NAVIGATION
-      .navigationViewStyle(.stack)
+    }
+    
+    //MARK: - CONTENT
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.clear
+                
+                //MARK: - PAGE IMAGE
+                Image(K.Image.magazineFrontCover)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
+                    .scaleEffect(imageScale)
+                //MARK: - 1. TAP GESTURE
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            resetImageState()
+                        }
+                    }
+                //MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
+            } //:ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            }
+            //MARK: - INFO PANEL
+            .overlay(alignment: .top) {
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+            }
+            
+        } //:NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
