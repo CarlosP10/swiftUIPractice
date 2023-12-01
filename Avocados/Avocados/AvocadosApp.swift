9d7779f300a6cdf4e1568b282d50e9c9582263a0
Carlos Paredes <00028516@uca.edu.sv>
2023-12-01 10:00:04 -0600

    [ADD] - Avocado App

---
+++ Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Carlos Paredes on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
